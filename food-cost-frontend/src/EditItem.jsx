8aab38c3aa6269c5327b08fd7a08f624e3c89286
Jake Spain <jake@thebagelbin.com>
2025-06-08 16:06:14 -0400

    add dropdown to edit item

--- food-cost-frontend/src/EditItem.jsx
+++ food-cost-frontend/src/EditItem.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import CostCell from './components/CostCell';
-import Select, { createFilter } from 'react-select';
+import Select from 'react-select';
+
 const API_URL = 'https://jaybird-connect.ue.r.appspot.com/api';
 
 function EditItem() {
