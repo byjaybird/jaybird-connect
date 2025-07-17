bc3ab29feb9d3c5bf9d71caa70d6e6f68cabb583
Jake Spain <jake@thebagelbin.com>
2025-07-17 08:58:03 -0400

    testing...

--- food-cost-frontend/src/ItemDetail.jsx
+++ food-cost-frontend/src/ItemDetail.jsx
@@ -63,7 +63,7 @@ function ItemDetail() {
           ✏️ Edit Item
         </Link>
           <button onClick={handleDownloadLabel} className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">
-            Get Label
+            Get A Label
           </button>
       </div>
       </div>
